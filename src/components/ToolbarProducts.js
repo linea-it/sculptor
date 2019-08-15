82bc5f604f938ff33e85465545dd59c2e3957f74
Matheus Freitas <mtsallein@gmail.com>
2019-08-15 15:39:10 -0300

    Update ToolbarProducts.js

--- src/components/ToolbarProducts.js
+++ src/components/ToolbarProducts.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/core/styles';
-import { Toolbar, Button } from '@material-ui/core';
+import { Toolbar } from '@material-ui/core';
 
 import IconButton from '@material-ui/core/IconButton';
 import Icon from '@material-ui/core/Icon';
