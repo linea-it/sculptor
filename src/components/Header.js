c9de0b382df67663eaf1eaef6c5d496cc7decc49
Fernando Nicolau <fernando_nicolau@outlook.com>
2020-03-05 16:38:34 -0300

    feat: redirection to the portal home via the logo and icon.

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,8 +4,8 @@ import { withStyles } from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import logo from '../assets/img/icon-des.png';
-import IconButton from '@material-ui/core/IconButton';
-import Typography from '@material-ui/core/Typography';
+import { Typography, IconButton, Button } from '@material-ui/core';
+import HomeIcon from '@material-ui/icons/Home';
 
 const styles = () => ({
   root: {
@@ -22,7 +22,15 @@ const styles = () => ({
   grow: {
     flexGrow: 1,
   },
+  separatorToolBar: {
+    flexGrow: 1,
+  },
 });
+
+const homeUrl =
+process.env.NODE_ENV === 'production'
+  ? window._env_.REACT_APP_HOME_URL
+  : process.env.REACT_APP_HOME_URL;
 
 function Header(props) {
   const { classes } = props;
@@ -31,12 +39,20 @@ function Header(props) {
       <div className={classes.root}>
         <AppBar position="static">
           <Toolbar>
-            <IconButton color="inherit" aria-label="Menu">
+            <IconButton href={homeUrl} color="inherit" aria-label="Menu">
               <img alt="logo DES" src={logo} />
             </IconButton>
             <Typography className={classes.grow} variant="h6" color="inherit">
               Science Products
             </Typography>
+            <div className={classes.separatorToolBar} />
+            <Button
+                color="inherit"
+                size="large"
+                href={homeUrl}
+            >
+              <HomeIcon /> 
+            </Button>
           </Toolbar>
         </AppBar>
       </div>
