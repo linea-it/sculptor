12485acafd155b283a58fd79f57f6dd6b2c8ac7b
Fernando Nicolau <fernando_nicolau@outlook.com>
2020-03-25 19:08:23 -0300

    feat: using windows location at uri da home.

--- src/components/Header.js
+++ src/components/Header.js
@@ -27,10 +27,9 @@ const styles = () => ({
   },
 });
 
-const homeUrl =
-process.env.NODE_ENV === 'production'
-  ? window._env_.REACT_APP_HOME_URL
-  : process.env.REACT_APP_HOME_URL;
+const homeUrl = `${window.location.protocol}//${window.location.hostname}${
+  window.location.port ? ':' : ''
+}${window.location.port}`;
 
 function Header(props) {
   const { classes } = props;
